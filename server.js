c0ff435fbdc19109b9f51d6cefcc9050def6bff6
azizzarrad <127695556+azizzarrad@users.noreply.github.com>
2023-06-29 21:27:30 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const port = 5000
+const app = express()
+const checkTime = require("./middleware");
+
+
+app.use(express.static("view"))
+app.use(checkTime)
+
+
+
+
+console.log(app)
+
+app.get("/", (req,res) =>{
+    res.status(200).sendFile(__dirname + "/view/home.html")
+})
+
+app.get("/oursevices", (req,res) =>{
+    res.status(200).sendFile(__dirname + "/view/ourServices.html")
+})
+
+
+app.get("/contact", (req,res) =>{
+    res.status(200).sendFile(__dirname + "/view/contactUs.html")
+})
+
+
+app.listen(port,(err) => err ? console.log("error: err") :
+ console.log("server is running on port :",port))
